3f33014c60fd8ef81120913bf123dee3fb517198
Aymen Mosrati <aymenmosrati66@gmail.com>
2026-03-03 03:33:40 +0000

    Changes

--- src/modules/UC05/pages/contentRequestDetail.tsx
+++ src/modules/UC05/pages/contentRequestDetail.tsx
@@ -1250,7 +1250,7 @@ const ContentRequestDetail: React.FC = () => {
           )}
 
           {activeTab === 'directives-log' && (
-            <div className="flex flex-col w-full rounded-2xl border border-[#E5E7EB] bg-white" dir="rtl">
+            <div className="flex flex-col w-full bg-white" dir="rtl">
               {/* Chat messages area */}
               <div className="flex-1 min-h-0">
                 {isLoadingConsultationRecords ? (
@@ -1380,7 +1380,7 @@ const ContentRequestDetail: React.FC = () => {
 
               {/* Inline chat input – consultant picker + question (like UC02) */}
               {meetingStatus !== MeetingStatus.RETURNED_FROM_CONTENT && meetingStatus !== MeetingStatus.SCHEDULED_ADDITIONAL_INFO && (
-                <div className="sticky bottom-0 z-10 border-t border-[#F3F4F6] bg-[#FAFAFA] rounded-b-2xl">
+                <div className="sticky bottom-[-24px] z-10 border-t border-[#F3F4F6] bg-[#FAFAFA] rounded-b-2xl -mx-6 -mb-6 mt-2">
                   {/* Consultant picker (expandable) */}
                   {showConsultantPicker && (
                     <div className="px-5 pt-4 pb-2 border-b border-[#F3F4F6]">
