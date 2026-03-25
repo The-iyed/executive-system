764fb5f5a0ff27492f26324e78a012c5feae3a69
Aymen Mosrati <aymenmosrati66@gmail.com>
2026-03-25 23:04:12 +0000

    Show scheduler fields in edit
    Co-authored-by: The-iyed <117833191+The-iyed@users.noreply.github.com>

--- src/modules/shared/features/meeting-request-form/submitter/Step1Form.tsx
+++ src/modules/shared/features/meeting-request-form/submitter/Step1Form.tsx
@@ -53,7 +53,7 @@ export function SubmitterStep1Form({ onSubmit, initialValues, isSchedulerEdit, m
             />
           )}
           <OnBehalfField />
-          {visibility.meeting_owner && <MeetingOwnerField
+          {(visibility.meeting_owner || isSchedulerEdit) && <MeetingOwnerField
               name="meeting_owner"
               initialLabel={initialValues?.meeting_owner?.name || initialValues?.meeting_owner?.username || initialValues?.meeting_owner?.displayName || initialValues?.meeting_owner?.mail }
             />}
