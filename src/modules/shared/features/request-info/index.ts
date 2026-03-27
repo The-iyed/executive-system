4f2f3c8e02b71f94bb5182488cdfee5f7ce6a039
aymenmosrati <aymenmosrati6@gmail.com>
2026-03-27 16:37:08 +0000

    Refactor UC02 meeting detail
    Co-authored-by: aymenmosrati <70782248+aymenmosrati@users.noreply.github.com>

--- src/modules/shared/features/request-info/index.ts
+++ src/modules/shared/features/request-info/index.ts
@@ -1,2 +1,4 @@
 export { RequestInfo } from "./RequestInfo";
-export type { RequestInfoData } from "./types";
+export type { RequestInfoData, RequestInfoProps } from "./types";
+export { mapMeetingToRequestInfo, resolveUserLabel } from "./requestInfoMapper";
+export type { RawMeetingForRequestInfo } from "./requestInfoMapper";
